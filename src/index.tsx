b7b1860bb56406ef957e067af9fa4d24695b3bb3
raojiajun-jag <471412450@qq.com>
2023-05-08 17:34:54 +0800

    feat: 发布到 npm

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 // export type { ColorfulButtonProps } from './components/colorful-button';
-// export { default as ColorfulButton } from './components/colorful-button';
+export { default as ColorfulButton } from './components/colorful-button';
 //
 // export type { ColorfulInputProps } from './components/colorful-input';
-// export { default as ColorfulInput } from './components/colorful-input';
+export { default as ColorfulInput } from './components/colorful-input';
 
 export { default as GateButton } from './components/gate-button/gate-button';
 
